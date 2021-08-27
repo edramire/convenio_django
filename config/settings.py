d77d703baa7d1785fbea8cbdcc9f57376e7b4bad
edramire <esteban.ramirez.b@usach.cl>
2021-08-27 00:18:56 -0400

    Cambia ubicacion de archivos estaticos

--- config/settings.py
+++ config/settings.py
@@ -148,8 +148,8 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/web/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'web/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
